69e1406838c82e99de77b32d1c0ed2969bc99192
Kshitiz . <kshitiz.@Kshitizs-MacBook-Air.local>
2024-05-22 01:10:37 +0530

    buyer api async fix

--- APIs/buyer-api/src/main/java/com/creovate/buyerapi3/Buyerapi3Application.java
+++ APIs/buyer-api/src/main/java/com/creovate/buyerapi3/Buyerapi3Application.java
@@ -1,6 +1,5 @@
 package com.creovate.buyerapi3;
 
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
@@ -11,25 +10,45 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 
-
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
 @SpringBootApplication
+@EnableAsync
 public class Buyerapi3Application {
 
     static final String hbaseIP = "34.93.220.20";
 
+    public static void main(String[] args) {
+        SpringApplication.run(Buyerapi3Application.class, args);
+    }
 
-	public static String hbaseConn(String pin){
-		Configuration config = HBaseConfiguration.create();
-        config.set("hbase.zookeeper.quorum", hbaseIP); 
-        config.set("hbase.zookeeper.property.clientPort", "2181"); 
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("HBaseLookup-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Async("taskExecutor")
+    public CompletableFuture<String> hbaseConn(String pin) {
+        Configuration config = HBaseConfiguration.create();
+        config.set("hbase.zookeeper.quorum", hbaseIP);
+        config.set("hbase.zookeeper.property.clientPort", "2181");
 
         try (Connection connection = ConnectionFactory.createConnection(config);
              Table table = connection.getTable(TableName.valueOf("pincode_serviceability"))) {
@@ -40,45 +59,42 @@ public class Buyerapi3Application {
 
             if (value != null) {
                 String merchants = Bytes.toString(value);
-                logger(" "+pin+"  -> "+merchants);
-                return merchants;
+                logger(" " + pin + "  -> " + merchants);
+                return CompletableFuture.completedFuture(merchants);
             } else {
-                logger("NO MERCHANTS FOUND FOR "+pin);
-                return "No Merchants Found";
+                logger("NO MERCHANTS FOUND FOR " + pin);
+                return CompletableFuture.completedFuture("No Merchants Found");
             }
 
         } catch (Exception e) {
             e.printStackTrace();
+            return CompletableFuture.completedFuture("Error: " + e.getMessage());
         }
-		return "Null";
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(Buyerapi3Application.class, args);
-	}
-
-
-    public static void logger(String txt){
-        System.out.println("########################################");
-        System.out.println();
-        System.out.println();
-        System.out.println("            "+txt+"                   ");
-        System.out.println();
-        System.out.println();
-        System.out.println("########################################");
-
     }
 
-	@RestController
+    public static void logger(String txt) {
+        System.out.println("########################################");
+        System.out.println();
+        System.out.println();
+        System.out.println("            " + txt + "                   ");
+        System.out.println();
+        System.out.println();
+        System.out.println("########################################");
+    }
+
+    @RestController
     public class HBaseController {
 
+        private final Buyerapi3Application application;
 
-        @GetMapping("/getMerchants")
-        public String getMerchants(@RequestParam String pincode) {
-            logger("GET REQUEST FOR "+pincode);
-			String merch = hbaseConn(pincode);
-			return merch;
+        public HBaseController(Buyerapi3Application application) {
+            this.application = application;
+        }
+
+        @GetMapping("/download")
+        public CompletableFuture<String> getMerchants(@RequestParam String pincode) {
+            logger("GET REQUEST FOR " + pincode);
+            return application.hbaseConn(pincode);
         }
     }
-
 }
